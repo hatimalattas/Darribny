1ae26e83d095982cf19b815bfe2b96325cbd8605
Hatim Alattas <56390393+hatimalattas@users.noreply.github.com>
2020-08-13 19:10:27 +0300

    designed registeration page

--- darribny/users/forms.py
+++ darribny/users/forms.py
@@ -1,52 +1,3 @@
-# from flask_wtf import FlaskForm
-# from wtforms import (StringField, BooleanField, DateTimeField,
-#                      RadioField,SelectField,TextField,
-#                      TextAreaField,SubmitField,IntegerField,DateField,PasswordField)
-# from wtforms.validators import DataRequired, Email, EqualTo
-# from wtforms import ValidationError
-# from flask_wtf.file import FileField, FileAllowed
-# from flask_login import current_user
-# from darribny.models import Trainee, Trainer
-
-# class LoginForm(FlaskForm):
-#     email = StringField('Email', validators=[DataRequired(),Email()])
-#     password = PasswordField('Password', validators=[DataRequired()])
-#     submit = SubmitField('Log In')
-
-# class RegistrationForm(FlaskForm):
-#     first_name = StringField('First Name', validators=[DataRequired()])
-#     last_name = StringField('Last Name', validators=[DataRequired()])
-#     mobile = IntegerField('Country Code', validators=[DataRequired()])
-#     city = RadioField('Select your city', choices=[('choice_one','Jeddah'),('choice_two','Riyadh'),('choice_three','Dammam')],validators=[DataRequired()])
-#     gender = RadioField('Select your gender', choices=[('choice_one','Male'),('choice_two','Female')],validators=[DataRequired()])
-#     birthdate = DateField('Birthdate', format='%Y-%m-%d',validators=[DataRequired()])
-#     email = StringField('Email', validators=[DataRequired(),Email()])
-#     username = StringField('Username', validators=[DataRequired()])
-#     password = PasswordField('Password', validators=[DataRequired(), EqualTo('pass_confirm', message='Passwords Must Match!')])
-#     pass_confirm = PasswordField('Confirm password', validators=[DataRequired()])
-#     submit = SubmitField('Register!')
-
-#     def check_email(self, field):
-#         if Trainee.query.filter_by(email=field.data).first():
-#             raise ValidationError('Your email has been registerd already!')
-#     def check_username(self, field):
-#         if Trainee.query.filter_by(username=field.data).first():
-#             raise ValidationError('Your username has been registerd already!')
-
-# class UpdateForm(FlaskForm):
-
-#     email = StringField('Email',validators=[DataRequired(),Email()])
-#     username = StringField('Username',validators=[DataRequired()])
-#     picture = FileField('Update Profile Picture',validators=[FileAllowed(['jpg','png'])])
-#     submit = SubmitField('Update')
-
-#     def check_email(self, field):
-#         if Trainee.query.filter_by(email=field.data).first():
-#             raise ValidationError('Your email has been registerd already!')
-#     def check_username(self, field):
-#         if Trainee.query.filter_by(username=field.data).first():
-#             raise ValidationError('Your username has been registerd already!')
-
 # Form Based Imports
 from flask_wtf import FlaskForm
 from wtforms import (StringField, BooleanField, DateTimeField,
@@ -73,9 +24,11 @@ class LoginForm(FlaskForm):
 class RegistrationForm(FlaskForm):
     first_name = StringField('First Name', validators=[DataRequired()])
     last_name = StringField('Last Name', validators=[DataRequired()])
-    mobile = IntegerField('Mobile', validators=[DataRequired()])
-    city = RadioField('Select your city', choices=[('choice_one','Jeddah'),('choice_two','Riyadh'),('choice_three','Dammam')],validators=[DataRequired()])
-    gender = RadioField('Select your gender', choices=[('choice_one','Male'),('choice_two','Female')],validators=[DataRequired()])
+    mobile = IntegerField('Mobile', validators=[DataRequired()], render_kw={"placeholder": "05xxxxxxxx"})
+    gender = SelectField('Gender',
+                          choices=[('NA','Select your gender'),('Male', 'Male'), ('Female', 'Female')])    
+    city = SelectField('City',
+                          choices=[('NA','Select your city'),('Jeddah', 'Jeddah'), ('Riyadh', 'Riyadh'), ('Dammam', 'Dammam')], render_kw={"placeholder": "Choose"})    
     birthdate = DateField('Birthdate', format='%Y-%m-%d',validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(),Email()])
     username = StringField('Username', validators=[DataRequired()])
