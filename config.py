e1ba18a9979cf39b9e23e290e7203ad0d682115a
Hatim Alattas <56390393+hatimalattas@users.noreply.github.com>
2020-09-22 20:58:18 +0300

    make it better

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # DEBUG = True
 
 # Connect to the database
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost:5432/darribny'
-# SQLALCHEMY_DATABASE_URI = 'postgres://bsqpwmqbhamiiz:b008118b47273a2e4e90b34bcebd50d60e8540943efdac841316e2058b90a19f@ec2-23-23-242-234.compute-1.amazonaws.com:5432/d17o8q0t643gbo'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost:5432/darribny'
+SQLALCHEMY_DATABASE_URI = 'postgres://bsqpwmqbhamiiz:b008118b47273a2e4e90b34bcebd50d60e8540943efdac841316e2058b90a19f@ec2-23-23-242-234.compute-1.amazonaws.com:5432/d17o8q0t643gbo'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
