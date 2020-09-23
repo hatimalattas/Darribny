847d4f975d1aa7679f6f7e0c28b1147b18f5a2de
Hatim Alattas <56390393+hatimalattas@users.noreply.github.com>
2020-09-23 14:35:24 +0300

    f

--- darribny/__init__.py
+++ darribny/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from commands import create_tables
 
 app = Flask(__name__ )
 
--- darribny/commands.py
+++ darribny/commands.py
@@ -1,7 +1,7 @@
 import click
 from flask.cli import with_appcontext
 
-from darribny.models import db, User, Reservation, Review
+from models import db, User, Reservation, Review
 
 @click.command(name='create_tables')
 @with_appcontext
